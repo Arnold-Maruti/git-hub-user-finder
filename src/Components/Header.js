1ef9178c6fc8c82a84cf7aa8c65b90c3543505fa
Alice Mugure <machariamugure17@gmail.com>
2025-04-22 12:26:04 +0300

    A summary description for the app

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>
+        Explore Github Profiles and Repositories
+      </h1>
+      <p>Discover GitHub users and their public projects with this simple, interactive app. 
+</p>
+     <p>Search for any GitHub username to view their profile details, pinned repositories, and recent activity—all in one place.
+</p>
+    </div>
+  )
+}
+
+export default Header
